1aa936fb42ed6b0326a5837c6f73bb6b151a31f3
skolorz <szym4j@gmail.com>
2015-04-12 01:07:12 -0700

    elem fix

--- src/MySchool.hs
+++ src/MySchool.hs
@@ -3,7 +3,7 @@ module MySchool where
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 removeNonUppercase :: [Char] -> [Char]
-removeNonUppercase str = [c | c <- str, c elem [A..Z] ]
+removeNonUppercase str = [c | c <- str, elem c ['A'..'Z'] ]
 
 main = do
     print (removeNonUppercase "uwaga uwaga JEDZIE")
