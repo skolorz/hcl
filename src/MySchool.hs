131506eea6c831fc6eb233b345f7808eb6b05194
Szymon Kolorz <szymon.kolorz@ingbank.pl>
2015-04-12 09:51:02 +0200

    removeNonUpperCase

--- src/MySchool.hs
+++ src/MySchool.hs
@@ -2,7 +2,7 @@ module MySchool where
 
 doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
-
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase str = [c | c <- str, c elem [A..Z] ]
 main = do
-    print "doubleUs"
-    print (doubleUs 2 1)
+    print (removeNonUppercase "uwaga uwaga JEDZIE")
