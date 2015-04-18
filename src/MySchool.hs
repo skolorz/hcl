9f35e94858a293acd4dcdf085224edea95d9f985
Szymon Kolorz <szymon.kolorz@ingbank.pl>
2015-04-18 16:10:11 +0200

    giignore

--- src/MySchool.hs
+++ src/MySchool.hs
@@ -4,6 +4,9 @@ doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase str = [c | c <- str, elem c ['A'..'Z'] ]
+lucky :: (Integral a) => a -> String
+lucky 7 = "You are lucky"
+lucky x = "Bad luck" 
 
 main = do
     print (removeNonUppercase "uwaga uwaga JEDZIE")
