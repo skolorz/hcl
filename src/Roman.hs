91f14dfd01818781fab5874632ce4e277e29d65b
Szymon Kolorz <szymon.kolorz@ingbank.pl>
2015-04-18 19:12:58 +0200

    refactor

--- src/Roman.hs
+++ src/Roman.hs
@@ -1,8 +1,12 @@
 module Roman where
 
+digits = [('M',1000),('D',500),('C',100),('L',50),('X',10),('V',5),('I',1)]
+isReducer d = elem d "IXC"
+rval x = head [v | (d,v) <- digits, x == d] 
+
 romanNumber :: Int -> String
+romanNumber 0 = ""
 romanNumber n
-    | n == 0 = ""
     | n >= 1000 = 'M' : romanNumber (n-1000)
     | n >= 900 = 'C' : romanNumber (n+100)
     | n >= 500 = 'D' : romanNumber (n-500)
@@ -16,10 +20,6 @@ romanNumber n
     | n >= 5 = 'V' : romanNumber (n-5)
     | n >= 4  = 'I' : romanNumber (n+1)
     | otherwise = 'I' : romanNumber (n-1) 
-
-digits = [('I',1),('V',5),('X',10),('L',50),('C',100),('D',500),('M',1000)]
-isReducer d = elem d "IXC"
-rval x = head [v | (d,v) <- digits, x == d] 
 
 readRoman :: String -> Int
 readRoman [] = 0
@@ -72,7 +72,8 @@ testRoman :: [(Int, String, String)]
 testRoman = [(d,r,result) | (d,r) <- good, let result = romanNumber d, r /= result]
 
 testReadRoman :: [(Int, String, Int)]
-testReadRoman = [(d,r,result) | d <- [1..10000], let r = romanNumber d, let result = readRoman r, d /= result]
-testDigitVaule = [(d,v,r) | (d,v) <- digits, let r = romanNumber v, [d] /= r]
+testReadRoman = [(d,r,result) | d <- [1..4000], let r = romanNumber d, let result = readRoman r, d /= result]
+testDigitValue = [(d,v,r) | (d,v) <- digits, let r = romanNumber v, [d] /= r]
+test = (testReadRoman,testRoman,testDigitValue)
 main = do
     print (romanNumber 1)
