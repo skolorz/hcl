284c983a0013e531d59a3ccd49ebde31ce5ef2b6
Szymon Kolorz <szymon.kolorz@ingbank.pl>
2015-04-21 22:12:24 +0200

    handle 4000, bugfix

--- src/Roman.hs
+++ src/Roman.hs
@@ -2,19 +2,21 @@ module Roman where
 
 digits = [('M',1000),('D',500),('C',100),('L',50),('X',10),('V',5),('I',1)]
 matchDigit :: Int -> (Char,Int)
-matchDigit n = head [(d,v) | (d,v) <- digits,  v <= n]
+possibleDigits n = [(d,v) | (d,v) <- digits,  v <= n]
+matchDigit n = head (possibleDigits n) 
+--prevDigit n | n < 5 = ("I",1)
+prevDigit n =  head (drop 1 (possibleDigits n ))
 isReducer d = elem d "IXC"
 rval x = head [v | (d,v) <- digits, x == d] 
 
 romanNumber :: Int -> String
 romanNumber 0 = ""
-romanNumber n
-    | n >= 900 && n < 1000 = 'C' : romanNumber (n+100)
-    | n >= 90 && n < 100 = 'X' : romanNumber (n+10)
-    | n == 9  = 'I' : romanNumber (n+1)
 romanNumber n 
-    | n >= (4 * v) && n < (5 * v)  =   d : romanNumber(n+v)
+    | isReducer d && n >= (4 * v) && n < (5 * v)  =   d : romanNumber(n+v)
     where  (d,v) = matchDigit n
+romanNumber n 
+    | n >=9 && n >= (9 * v) && n < (10 * v) && isReducer d =   d : romanNumber(n+v)
+    where  (d,v) = prevDigit n
 romanNumber n = let (d,v) = matchDigit n in d:romanNumber (n-v) 
 
 readRoman :: String -> Int
@@ -65,10 +67,10 @@ good = [
     ]
     
 testRoman :: [(Int, String, String)]
-testRoman = [(d,r,result) | (d,r) <- good, let result = romanNumber d, r /= result]
+testRoman = take 100 [(d,r,result) | (d,r) <- good, let result = romanNumber d, r /= result]
 
 testReadRoman :: [(Int, String, Int)]
-testReadRoman = [(d,r,result) | d <- [1..4000], let r = romanNumber d, let result = readRoman r, d /= result]
+testReadRoman = take 100 [(d,r,result) | d <- [1..3900], let r = romanNumber d, let result = readRoman r, d /= result]
 testDigitValue = [(d,v,r) | (d,v) <- digits, let r = romanNumber v, [d] /= r]
 test = (testReadRoman,testRoman,testDigitValue)
 main = do
